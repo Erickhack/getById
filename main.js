d4677396e7ce2bc61d316fc12709120f0b8b38b5
ErickHack <blackdragoneraj@gmail.com>
2021-09-01 15:43:21 +0500

    v2, firtServer

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const port = 9999;
 const statusBadReq = 400;
 const statusNotFaund = 404;
 const statusOk = 200;
+const errorHtml = `
+<h1>Not Found</h1>
+<p>The requested URL was not found on this server.</p>
+<hr/>
+<i>Apache/2.4.47 (Win64) OpenSSL/1.1.1k PHP/7.4.19 Server at localhost Port 80</i>
+`;
 
 let nextId = 1;
 const posts = [];
@@ -19,11 +25,12 @@ function sendResponse(res, {status = statusOk, headers = {}, body = null}) {
 }
 
 function sendJSON(res, body) {
+    const space = 2;
     sendResponse(res, {
         headers: {
-            'Content-Type': 'application/sjon',
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify(body),
+        body: JSON.stringify(body, null, space),
     });
 }
 
@@ -32,35 +39,53 @@ const methods = new Map();
 methods.set('/posts.get', ({res}) => {
     sendJSON(res, posts);
 });
-methods.set('/posts.getById', ({}) => {
+methods.set('/posts.getById', ({res, searchParams}) => {
+    if (!searchParams.has('id')) {
+        sendResponse(res, {status: statusNotFaund, body: errorHtml});
+        return;
+    }
+    
+    const id = searchParams.get('id');
+
+    if (id === undefined || isNaN(id) || posts.length === 0) {
+        sendResponse(res, {status: statusBadReq, body: errorHtml});
+        return;
+    }
+    const idPost = posts.filter(o => o.id === parseInt(id, 10));
+    const okHtml = `
+    <h1>Ok</h1>
+    <p>${JSON.stringify(idPost)}</p>
+    <hr/>
+    <i>Apache/2.4.47 (Win64) OpenSSL/1.1.1k PHP/7.4.19 Server at localhost Port 80</i>
+    `;
+    sendJSON(res, idPost);
 });
 methods.set('/posts.post', ({res, searchParams}) => {
     if (!searchParams.has('content')) {
-        sendResponse(res, {status: statusBadReq});
+        sendResponse(res, {status: statusBadReq, body: errorHtml});
         return;
     }
 
     const content = searchParams.get('content');
-    console.log(content);
 
     const post = {
-        if: nextId++,
+        id: nextId++,
         content: content,
         created: Date.now(),
-    }
+    };
 
     posts.unshift(post);
-    sendJSON(res, post)
+    sendJSON(res, post);
 });
 methods.set('/posts.edit', ({}) => {});
 methods.set('/posts.delete', ({}) => {});
 
 http.createServer((req, res) => {
-    const {pathname, searchParams}  = new URL(req.url, `http://${req.headers.host}`);
+    const {pathname, searchParams} = new URL(req.url, `http://${req.headers.host}`);
     const method = methods.get(pathname);
 
     if (method === undefined) {
-        sendResponse(res, {status: statusNotFaund});
+        sendResponse(res, {status: statusNotFaund, body: errorHtml});
         return;
     }
 
@@ -69,7 +94,7 @@ http.createServer((req, res) => {
         res,
         pathname,
         searchParams,
-    }
+    };
 
     method(params);
 }).listen(port);
